6deff68c7947bc8329196786e60af51487dab931
Dima <dmitry.korolev@gmail.com>
2015-05-10 23:56:34 -0700

    Branched out `html.h`.

--- src/code.cc
+++ src/code.cc
@@ -242,7 +242,7 @@ int main(int argc, char** argv) {
 #include <string>
 #include <utility>
 
-#include <initializer_list>
+#include "html.h"
 
 #include "../Bricks/net/api/api.h"
 #include "../Bricks/dflags/dflags.h"
@@ -251,119 +251,6 @@ int main(int argc, char** argv) {
 
 DEFINE_int32(port, 8687, "Port to spawn the secret server on.");
 DEFINE_string(route, "/secret", "The route to serve the dashboard on.");
-
-namespace html {
-
-using bricks::ThreadLocalSingleton;
-
-struct State {
-  std::string html;
-  enum { NONE, IN_PROGRESS, COMMITTED } state = NONE;
-  void Begin() {
-    assert(state == NONE);
-    html = "<!doctype html>\n";
-    state = IN_PROGRESS;
-  }
-  std::string Commit() {
-    assert(state == IN_PROGRESS);
-    state = COMMITTED;
-    return html;
-  }
-  template <typename T>
-  void Append(T&& content) {
-    assert(state == IN_PROGRESS);
-    html.append(content);
-  }
-  void End() {
-    assert(state == COMMITTED);
-    state = NONE;
-  }
-};
-
-// TODO(dkorolev): Eliminate some copy-pasting.
-// TODO(dkorolev): Escaping?
-
-struct HTML final {
-  HTML() { ThreadLocalSingleton<State>().Begin(); }
-  ~HTML() { ThreadLocalSingleton<State>().End(); }
-  std::string AsString() { return ThreadLocalSingleton<State>().Commit(); }
-};
-
-#if defined(SCOPED_TAG) || defined(TEXT_TAG) || defined(SHORT_TAG)
-#error "`SCOPED_TAG`, `TEXT_TAG` and `SHORT_TAG` should not be defined by here."
-#endif
-
-#define SCOPED_TAG(tag)                                                                   \
-  struct tag {                                                                            \
-    tag() { ThreadLocalSingleton<State>().Append("<" #tag ">"); }                         \
-    template <typename T>                                                                 \
-    void construct(T&& params) {                                                          \
-      auto& html = ThreadLocalSingleton<State>();                                         \
-      html.Append("<" #tag);                                                              \
-      for (const auto cit : params) {                                                     \
-        html.Append(" ");                                                                 \
-        html.Append(cit.first);                                                           \
-        html.Append("='");                                                                \
-        html.Append(cit.second);                                                          \
-        html.Append("'");                                                                 \
-      }                                                                                   \
-      html.Append(">");                                                                   \
-    }                                                                                     \
-    tag(const std::vector<std::pair<std::string, std::string>>& v) { construct(v); }      \
-    tag(std::initializer_list<std::pair<std::string, std::string>> il) { construct(il); } \
-    ~tag() { ThreadLocalSingleton<State>().Append("</" #tag ">"); }                       \
-  }
-
-#define TEXT_TAG(tag)                                               \
-  struct tag {                                                      \
-    tag() { ThreadLocalSingleton<State>().Append("<" #tag ">"); }   \
-    tag(const std::string& content) {                               \
-      ThreadLocalSingleton<State>().Append("<" #tag ">");           \
-      ThreadLocalSingleton<State>().Append(content);                \
-    }                                                               \
-    ~tag() { ThreadLocalSingleton<State>().Append("</" #tag ">"); } \
-  }
-
-#define SHORT_TAG(tag)                                                                    \
-  struct tag {                                                                            \
-    tag() = delete;                                                                       \
-    template <typename T>                                                                 \
-    void construct(T&& params) {                                                          \
-      auto& html = ThreadLocalSingleton<State>();                                         \
-      html.Append("<" #tag);                                                              \
-      for (const auto cit : params) {                                                     \
-        html.Append(" ");                                                                 \
-        html.Append(cit.first);                                                           \
-        html.Append("='");                                                                \
-        html.Append(cit.second);                                                          \
-        html.Append("'");                                                                 \
-      }                                                                                   \
-      html.Append(" />");                                                                 \
-    }                                                                                     \
-    tag(const std::vector<std::pair<std::string, std::string>>& v) { construct(v); }      \
-    tag(std::initializer_list<std::pair<std::string, std::string>> il) { construct(il); } \
-  }
-
-SCOPED_TAG(HEAD);
-SCOPED_TAG(BODY);
-
-TEXT_TAG(TITLE);
-TEXT_TAG(P);
-TEXT_TAG(PRE);
-
-SCOPED_TAG(TABLE);
-SCOPED_TAG(TR);
-SCOPED_TAG(TD);
-
-SCOPED_TAG(A);
-
-SHORT_TAG(IMG);
-
-#undef SCOPED_TAG
-#undef TEXT_TAG
-#undef SHORT_TAG
-
-}  // namespace html
 
 int main(int argc, char** argv) {
   ParseDFlags(&argc, &argv);
@@ -398,7 +285,7 @@ int main(int argc, char** argv) {
           }
         }
         {
-          std::vector<std::pair<std::string, std::string>> v{{"align", "center"}};
+          std::vector<std::pair<std::string, std::string> > v{{"align", "center"}};
           TR r(v);
           {
             TD d;
